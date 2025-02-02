d79059f10c6c905a1f5686eaaa7047112a0f0c4a
Tahsin Abdullah <tahsinabdullah107@gmail.com>
2025-02-02 16:45:11 +0600

    update

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -85,6 +85,39 @@ const toggleSidebar = () => {
 <template>
     <div class="min-h-screen bg-gray-50">
         <!-- Top Navigation Bar -->
+
+
+
+
+
+
+        <button
+      @click="toggleSidebar"
+      class="fixed top-4 left-4 z-50 lg:hidden p-2 rounded-md bg-white shadow-lg"
+    >
+      <svg
+        class="w-6 h-6"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          v-if="!isSidebarOpen"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+        <path
+          v-else
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </button>
+
         <nav class="bg-white border-b border-gray-200">
     <div class="mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 justify-between">
@@ -251,14 +284,6 @@ const toggleSidebar = () => {
                     </a>
                 </nav>
       </aside>
-
-
-
-
-
-
-
-
 
 
 
